e791228485abb75273a20c69a48164bb9f040dda
Avijit <abhijitpradhan909@gmail.com>
2024-02-26 19:50:58 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const btn = document.getElementById("translateBtn");
+btn.addEventListener("click", async () => {
+  const inputText = document.getElementById("inputText").value;
+  const sourceLanguage = document.getElementById("sourceLanguage").value;
+  const targetLanguage = document.getElementById("targetLanguage").value;
+  const result = document.getElementById("outputText");
+
+  const url = `https://api.mymemory.translated.net/get?q=${inputText}&langpair=en|${targetLanguage}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const translateddata = data["matches"][0].translation;
+    result.innerText = translateddata
+    console.log(translateddata);
+  } catch (error) {
+    
+  }
+
+});
