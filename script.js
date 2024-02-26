fd271e4fbe90fd7957a98822a8d16e1727eba3ab
Avijit <abhijitpradhan909@gmail.com>
2024-02-26 20:25:24 +0530

    fix all bugs

--- script.js
+++ script.js
@@ -5,15 +5,13 @@ btn.addEventListener("click", async () => {
   const targetLanguage = document.getElementById("targetLanguage").value;
   const result = document.getElementById("outputText");
 
-  const url = `https://api.mymemory.translated.net/get?q=${inputText}&langpair=en|${targetLanguage}`;
+  const url = `https://api.mymemory.translated.net/get?q=${inputText}&langpair=${sourceLanguage}|${targetLanguage}`;
   try {
     const response = await fetch(url);
     const data = await response.json();
     const translateddata = data["matches"][0].translation;
-    result.innerText = translateddata
-    console.log(translateddata);
+    result.innerText = translateddata;
   } catch (error) {
-    
+    console.error(error)
   }
-
 });
